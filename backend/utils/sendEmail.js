69a96c4bc0929430871d29297434e44f02d48f8c
pratham13103 <167630988+pratham13103@users.noreply.github.com>
2025-01-20 15:53:33 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,1 +1,30 @@
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+    host: 'email-smtp.eu-north-1.amazonaws.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'AKIAZQ3DTEXNDEG37UF3',  // SES SMTP user
+        pass: 'BHdNdhRBfXKX7nbh+lfzgsQQcfAUFjGeSdd1tZcNnlX7'   // SES SMTP password
+    }
+});
+
+const sendEmail = (to, subject, text) => {
+    const mailOptions = {
+        from: 'prathamesh.r.jaiswal@gmail.com',  // Verified SES sender email
+        to: to,
+        subject: subject,
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+};
+
+module.exports = sendEmail;
